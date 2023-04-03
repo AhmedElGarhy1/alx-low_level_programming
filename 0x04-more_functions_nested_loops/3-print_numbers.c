a358bbe7acc7ddae03facd4408f38ad6b8c78b15
AhmedElGarhy1 <gemater.g@gmail.com>
2023-04-03 07:32:43 -0700

    updates

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * print_numbers - returns from 1 to 9
+  */
+
+void print_numbers(void)
+{
+	for (int i=0; i<10; i++)
+		_putchar(i);
+	_putchar("\n");
+}
