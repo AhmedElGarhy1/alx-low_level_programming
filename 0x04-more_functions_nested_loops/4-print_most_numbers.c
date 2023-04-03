204f11c0e4c666fb6ccae50cbf7f2a8cc36799b2
AhmedElGarhy1 <gemater.g@gmail.com>
2023-04-03 08:43:55 -0700

    updates

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * print_most_numbers - returns from 1 to 9
+  */
+
+void print_numbers(void)
+{
+	char c;
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 4 || i == 2)
+			continue;
+		c = i + 48;
+		_putchar(c);
+	}
+	c = '\n';
+	_putchar(c);
+}
