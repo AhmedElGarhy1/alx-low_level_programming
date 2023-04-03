719ef4328958367b218f9871bda2eda9db1fec65
AhmedElGarhy1 <gemater.g@gmail.com>
2023-04-03 07:14:37 -0700

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul  - check the code
+ * @a: first number
+ * @b: second number
+ * Return: their multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
