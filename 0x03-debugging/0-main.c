2d39c34a67a4759f3e5216cefeb566db82e8a0b9
AhmedElGarhy1 <gemater.g@gmail.com>
2023-04-03 05:55:38 -0700

    update headerfile

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "1-main.h"
 
 /**
   * main - tests function that prints if integer is positive or negative
---
+++ 0x03-debugging/0-main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+{
+	if(i >= 0)
+		printf("%d is positive\n");
+	else if(i < 0)
+ 		printf("%d is negative\n");
+}
+#endif /* MAIN_H */
